e42ff2a6179d544db7fa075a7b0a24a378a236d5
josesg998 <josesg998@gmail.com>
2023-08-22 23:19:15 -0300

    score: 45.80296 max_depth 7 y min_sample split = 0 y minbucket= 2  con seed = 290497

--- src/rpart/z101_PrimerModelo.r
+++ src/rpart/z101_PrimerModelo.r
@@ -24,8 +24,8 @@ modelo <- rpart(
         data = dtrain, # los datos donde voy a entrenar
         xval = 0,
         cp = -0.3, # esto significa no limitar la complejidad de los splits
-        minsplit = 5, # minima cantidad de registros para que se haga el split
-        minbucket = 1, # tamaño minimo de una hoja
+        minsplit = 0, # minima cantidad de registros para que se haga el split
+        minbucket = 2, # tamaño minimo de una hoja
         maxdepth = 7
 ) # profundidad maxima del arbol
 
