40d9dfe32ea3baf2e02d77cc56e5647bb1d0026c
josesg998 <josesg998@gmail.com>
2023-09-09 19:26:01 -0300

    muestras de IPC con top 2 de BO public

--- src/rpart/arbol_con_oversampling.R
+++ src/rpart/arbol_con_oversampling.R
@@ -8,15 +8,13 @@ require("rpart.plot")
 
 # Aqui se debe poner la carpeta de la materia de SU computadora local
 #setwd("X:\\gdrive\\uba2023\\") # Establezco el Working Directory
-#setwd("~/Data Mining/DM_EyF_23/dmeyf2023")
+setwd("~/Data Mining/DM_EyF_23/dmeyf2023")
 
 # cargo el dataset
-dataset <- fread("datasets/competencia_01.csv")
+dataset <- fread("../datasets/competencia_01.csv")
 
 dataset[ , clase_binaria := ifelse( clase_ternaria=="CONTINUA", "NEG", "POS" ) ]
 
-#dataset[,caja_ahorro_bin := ifelse(mcaja_ahorro>=0,1,0)]
-dataset[,ctrx_quarter_bin:=ifelse(ctrx_quarter>=48,1,0)]
 
 dtrain <- dataset[foto_mes == 202103] # defino donde voy a entrenar
 dapply <- dataset[foto_mes == 202105] # defino donde voy a aplicar el modelo
@@ -26,10 +24,10 @@ pesos <- copy( dtrain[, ifelse( clase_ternaria=="CONTINUA",   1.0, 100.0  ) ] )
 param <- list()
 
 param$cp <-  -1 # esto significa no limitar la complejidad de los splits
-param$minsplit <-  725 # minima cantidad de registros para que se haga el split
-param$minbucket <-  212 # tamaño minimo de una hoja
+param$minsplit <-  659 # minima cantidad de registros para que se haga el split
+param$minbucket <-  200 # tamaño minimo de una hoja
 param$maxdepth <-  12 # profundidad maxima del arbol
-param$corte <- 9230
+param$corte <- 9503
 
 
 # genero el modelo,  aqui se construye el arbol
@@ -44,8 +42,15 @@ modelo <- rpart(
 
 
 # grafico el arbol
-#prp(modelo,extra = 101, digits = -5,branch = 1, type = 4, varlen = 0, faclen = 0)
+prp(modelo,extra = 101, digits = -5,branch = 1, type = 4, varlen = 0, faclen = 0)
 
+#comparo importancias con etiuquetas
+diccionario <- data.table(readODS::read_ods("../datasets/DiccionarioDatos_2023.ods"))
+
+importancias <- data.table(importance=as.vector(modelo$variable.importance),
+                           campo=as.vector(names(modelo$variable.importance)))
+
+evaluacion_importancias <- merge(diccionario,importancias)
 
 # aplico el modelo a los datos nuevos
 prediccion <- predict(
