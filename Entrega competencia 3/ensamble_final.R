a12c73f8cf5d680845a70f61b35d0b54c9e9d71e
josesg998 <josesg998@gmail.com>
2023-11-29 13:56:26 -0300

    comento para referenciar

--- Entrega competencia 3/ensamble_final.R
+++ Entrega competencia 3/ensamble_final.R
@@ -1,12 +1,21 @@
 setwd('~/buckets/b1')
 require("data.table")
 
+# entrega de Pengue y Marchesini en competencia 02
 pengue <- fread("exp/KA8240_03_pengue_marchesini/prediccion.txt")
+# entrega de Casali en competencia 02
 casalli <- fread("exp/final_casalli/prediccion.txt")
+# entrega de Serpa en competencia 02
 serpa <- fread('exp/KA8240_serpa/prediccion.txt')
+# Entrega de Flores
 flores <- fread('exp/KA5240_vanesa_flores/prediccion.txt')
+# Experimento de lags de 
 lags <- fread('exp/exp_final/V4_2_prob.csv')[,list(numero_de_cliente,foto_mes,prob)]
+
+# Experimento de corrección de variables rotas de Serpa y Saint Germain
+## Con muchos meses
 moda2 <- fread('exp/KA8240_mode2/prediccion.csv')[,list(numero_de_cliente,foto_mes,prob)]
+## Con 7 meses
 moda <- fread('exp/KA8240_mode/prediccion.csv')[,list(numero_de_cliente,foto_mes,prob)]
 
 df <- rbind(pengue,
